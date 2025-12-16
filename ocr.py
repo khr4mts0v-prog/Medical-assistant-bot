326381237ec36c53dbbc4b34a7ae0e36675adfd6
khr4mts0v-prog <khr4mts0v@gmail.com>
2025-12-16 21:22:19 +0300

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,10 +1,14 @@
 import pytesseract
+from PIL import Image
 import logging
 
-def ocr_file(file_path):
+def ocr_image(path: str) -> str:
     try:
-        text = pytesseract.image_to_string(file_path, lang="rus")
-        return text
+        text = pytesseract.image_to_string(
+            Image.open(path),
+            lang="rus"
+        )
+        return text.strip()
     except Exception as e:
         logging.error(f"OCR error: {e}")
         return ""
